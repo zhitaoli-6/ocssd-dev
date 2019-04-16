dc620de869dbea8b4720a7899de0f6a3b8c160f9
Zhitao Li <1318078301@qq.com>
2019-04-16 21:22:12 +0800

    only show stats: req_writes, sub_writes and read

--- ocssdr/pblk-sysfs.c
+++ ocssdr/pblk-sysfs.c
@@ -414,6 +414,12 @@ static ssize_t pblk_sysfs_get_padding_dist(struct pblk *pblk, char *page)
 static ssize_t pblk_sysfs_stats_debug(struct pblk *pblk, char *page)
 {
 	return snprintf(page, PAGE_SIZE,
+		"%ld\t%ld\t%lu\n",
+			atomic_long_read(&pblk->req_writes),
+			atomic_long_read(&pblk->sub_writes),
+			atomic_long_read(&pblk->sync_reads));
+	/*
+	return snprintf(page, PAGE_SIZE,
 		"%lu\t%lu\t%ld\t%llu\t%ld\t%lu\t%lu\t%lu\t%lu\t%lu\t%lu\t%lu\t%lu\n",
 			atomic_long_read(&pblk->inflight_writes),
 			atomic_long_read(&pblk->inflight_reads),
@@ -428,6 +434,7 @@ static ssize_t pblk_sysfs_stats_debug(struct pblk *pblk, char *page)
 			atomic_long_read(&pblk->recov_gc_reads),
 			atomic_long_read(&pblk->cache_reads),
 			atomic_long_read(&pblk->sync_reads));
+			*/
 }
 #endif
 
